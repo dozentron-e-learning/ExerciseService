b3990a800f17df0595a8cdd0e46ed4ef29dd8837
Dominic Althaus <dominic.althaus@mni.thm.de>
2019-07-06 19:48:17 +0200

    define more fields on exerciseService

--- app/models/api/v1/exercise.rb
+++ app/models/api/v1/exercise.rb
@@ -2,6 +2,10 @@ class Api::V1::Exercise
   include Mongoid::Document
   include Mongoid::Paperclip
 
+  VALIDATION_NOT_PERFORMED = :not_performed
+  VALIDATION_SUCCEEDED = :succeeded
+  VALIDATION_FAILED = :failed
+
   STRONG_PARAMETERS = %i[
     title
     description
@@ -11,6 +15,12 @@ class Api::V1::Exercise
     exercise_test
     exercise_hidden_test
     exercise_stub
+    validation_status
+    test_validation_error
+    hidden_test_validation_error
+    stub_validation_error
+    general_validation_error
+    general_validation_error_details
   ].freeze
 
   has_mongoid_attached_file :exercise_test
@@ -26,7 +36,12 @@ class Api::V1::Exercise
   field :deadline, type: Date
   field :visible_date, type: Date
   field :do_plagiarism_check, type: Boolean, default: false
-  field :exercise_validated, type: Boolean, default: false
+  field :validation_status, type: Symbol, default: VALIDATION_NOT_PERFORMED
+  field :test_validation_error, type: String
+  field :hidden_test_validation_error, type: String
+  field :stub_validation_error, type: String
+  field :general_validation_error, type: String
+  field :general_validation_error_details, type: String
 
   validates :title, length: { minimum: 1 }
   validates :exercise_test, presence: true
