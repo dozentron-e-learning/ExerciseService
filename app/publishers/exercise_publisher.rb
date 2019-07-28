1a260e9d2eecae5613d826053963d2e88f699cad
Dominic Althaus <dominic.althaus@mni.thm.de>
2019-07-28 21:11:52 +0200

    add logging to show when we send message

--- app/publishers/exercise_publisher.rb
+++ app/publishers/exercise_publisher.rb
@@ -1,5 +1,6 @@
 class ExercisePublisher < ActiveBunny::Publisher
   def create(exercise)
+    Rails.logger.info "Send Message about new Exercise: #{exercise.id.to_s}"
     #TODO Generate token to use in controller
     publish({id: exercise.id.to_s, validation_token: ""}.to_json)
   end
